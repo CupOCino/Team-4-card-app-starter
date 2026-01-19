c37a6282773d3f4e8a00f45de10b8b631ecdb9e9
Your Name <youremail@yourdomain.com>
2026-01-19 11:07:20 +0800

    form

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -6,12 +6,29 @@ export default function CardForm({
   error,
   submitText,
 }) {
-  /* TODO: Complete the CardForm component 
+    /* TODO: Complete the CardForm component 
   - display form inputs for card_name and card_pic
   - display error message
-  - display submit button 
-  - handle form submission 
+  - display submit button ----
+  - handle form submission ----
   - style as a form UI */
+  return (
+    <>
+      <h1>Add a new card</h1>
 
-  return <form></form>;
+      <form onSubmit={onSubmit}>
+        <label>
+          Card Name:
+          <input type="text" name="name" />
+        </label>
+
+        <label>
+          Card Picture:
+          <input type="text" name="picture" />
+        </label>
+        
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+  );
 }
