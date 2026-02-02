78e3111ee349ddf331aa7e590fed562207fe2310
JJ <jiajieloh1112@gmail.com>
2026-02-03 07:03:32 +0800

    login route

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/allassignments" element={<AssignmentList />} />
         <Route path="/addassignment" element={<AddAssignment />}/>
